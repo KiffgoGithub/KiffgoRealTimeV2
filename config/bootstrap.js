2aefaf34ce138c8bc384840962f899189c365354
Anas Ashraf <anas.ashraf@viabletree.com>
2020-04-22 12:53:57 +0500

    testing socket id to create room

--- config/bootstrap.js
+++ config/bootstrap.js
@@ -27,8 +27,11 @@ module.exports.bootstrap = async function () {
   // ```
 
   sails.io.on("connect", function (socket) {
-    socket.on("test", function (soc) {
-      console.log({ socketID_hai: socket.id });
+    socket.on("business", function (soc) {
+      console.log({ business_socketID: socket.id });
+    });
+    socket.on("kiffgo", function (soc) {
+      console.log({ kiffgo_socketID: socket.id });
     });
   });
 };

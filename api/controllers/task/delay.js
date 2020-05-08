b2fc0fc750f205a2e01ac3c806e7ef8a481cca6b
Anas Ashraf <anas.ashraf@viabletree.com>
2020-05-08 12:14:57 +0500

    testing

--- api/controllers/task/delay.js
+++ api/controllers/task/delay.js
@@ -18,16 +18,28 @@ module.exports = {
 
   fn: async function (inputs, exits) {
     try {
-      const roomInfo = await SocketInfo.findOne({ userId: inputs.businessId });
-      socketRooms = ["kiffgo"];
+      let socketRooms = ["kiffgo"];
 
-      if (roomInfo && roomInfo.roomName !== "kiffgo") {
-        socketRooms.push(roomInfo.roomName);
+      const kiffgoRoomInfo = await SocketInfo.find({
+        roomName: "kiffgo",
+      });
+      if (kiffgoRoomInfo) {
+        for (let i = 0; i < kiffgoRoomInfo.length; i++) {
+          sails.sockets.join(
+            kiffgoRoomInfo[i].socketId,
+            kiffgoRoomInfo[i].roomName
+          );
+        }
       }
 
-      //sails.sockets.blast("test", { location, userId, deliveryId });
-      console.log(roomInfo);
-      sails.sockets.join(roomInfo.socketId, roomInfo.roomName);
+      const roomInfo = await SocketInfo.find({
+        userId: inputs.businessId,
+      }).limit(1);
+      if (roomInfo && roomInfo[0].roomName !== "kiffgo") {
+        socketRooms.push(roomInfo[0].roomName);
+        sails.sockets.join(roomInfo[0].socketId, roomInfo[0].roomName);
+      }
+
       sails.sockets.broadcast(socketRooms, "taskDelay", {
         task: inputs.task,
       });

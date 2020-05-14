6ec96c66dd3141b7d6c0a54a74d751a4f84a4727
Anas Ashraf <anas.ashraf@viabletree.com>
2020-05-14 06:51:24 +0500

    checking problem in emit of new task added

--- api/controllers/task/new-task-added.js
+++ api/controllers/task/new-task-added.js
@@ -22,7 +22,7 @@ module.exports = {
     try {
       const socketRooms = await sails.helpers.joinRoom(inputs.businessId);
 
-      sails.sockets.blast("taskAdded", {
+      sails.sockets.broadcast(socketRooms, "taskAdded", {
         task: inputs.task,
       });
     } catch (err) {

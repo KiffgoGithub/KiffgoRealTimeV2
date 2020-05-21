02510a911b7877f350884a4a9b4f476083d38c00
Anas Ashraf <anas.ashraf@viabletree.com>
2020-05-21 14:29:33 +0500

    fixed

--- api/controllers/task/task-updated.js
+++ api/controllers/task/task-updated.js
@@ -25,7 +25,7 @@ module.exports = {
     try {
       const socketRooms = await sails.helpers.joinRoom(inputs.businessId);
 
-      sails.sockets.broadcast(socketRooms, inputs.event, input.emitData);
+      sails.sockets.broadcast(socketRooms, inputs.event, inputs.emitData);
     } catch (err) {
       sails.log.error(
         "task-updated action:  Task could not be updated due to this err: ",
